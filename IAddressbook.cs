c4c907cb5d56e5fa933e2037cf7d3e59458538c9
Janu shankar <janakiharishankar24@gmail.com>
2022-01-18 23:00:06 +0530

    commit a file

---
+++ IAddressbook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RefactortheAddressBook
+{
+    internal interface IAddressbook
+    {
+        public void addMultipleContact(string firstname, string lastname, string address, string city, string state, string zipcode, string phonenumber, string email);
+
+        public void multipleContact();
+    }
+
+}
