6ba62b381da01613ad9fa2d3d0caec50746354e6
Janu shankar <janakiharishankar24@gmail.com>
2022-01-18 22:58:53 +0530

    commit a file

---
+++ Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactortheAddressBook
+{
+    public class Address:IAddressbook
+    {
+        private LinkedList<Addcontact> Addcontactlist;
+        private Dictionary<string, Addcontact> Addcontactmap;
+    public Address()
+        {
+            this.Addcontactlist = new LinkedList<Addcontact>();
+            this.Addcontactmap = new Dictionary<string,Addcontact>();
+
+        }
+        public void addMultipleContact(string firstname, string lastname, string address, string city, string state, string zipcode, string phonenumber, string email)
+        {
+            Addcontact addcontact = new Addcontact(firstname, lastname, address, city, state, zipcode, phonenumber, email);
+            this.Addcontactlist.AddLast(addcontact);
+            this.Addcontactmap.Add(firstname, addcontact);
+        }
+        public void multipleContact()
+        {
+            foreach (Addcontact addcontact in this.Addcontactlist)
+            {
+                Console.WriteLine(addcontact.ToString());
+            }
+        
+        }
+
+    }
+}
